15e0958b241cac7a663ad9226c95ee50f4640b43
Андрій Борсук <johnburitto@gmail.com>
2023-04-01 18:27:19 +0300

    Add RecycleView to display all estate properties

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -35,13 +35,13 @@ class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
     private val _response = MutableLiveData<String>()
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
     // The external immutable LiveData for the response String
     val response: LiveData<String>
         get() = _response
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -56,13 +56,8 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsRealEstateProperties() {
         viewModelScope.launch {
             try {
-                val resultList = MarsApi.retrofitService.getProperties()
-
-                _response.value = "Success: ${resultList.size} Mars properties retrieved"
-
-                if (resultList.isNotEmpty()) {
-                    _property.value = resultList[0]
-                }
+                _properties.value = MarsApi.retrofitService.getProperties()
+                _response.value = "Success: Mars properties retrieved"
             }
             catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
